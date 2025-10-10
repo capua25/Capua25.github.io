63b492d780dff7f2d3fcf1983f847bd10b02e032
Martín Capuano <capuano.martin@outlook.com>
2025-10-10 11:05:32 -0300

    migrando a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://capua25.github.io',
+  // site: 'https://capua25.github.io',
+  site: 'https://martin-capuano.vercel.app',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
