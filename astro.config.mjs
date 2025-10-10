12d17a1020f68be3c6b0b3832302db45fe12ff40
Martín Capuano <capuano.martin@outlook.com>
2025-10-10 11:26:06 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://capua25.github.io',
-  site: 'https://martin-capuano.vercel.app',
+  site: 'https://martin-capuano.vercel.app/en',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
