345f5d290a5f18281524100bac7e31f199c83dd7
capua25 <capuano.martin@outlook.com>
2025-10-12 18:22:57 -0300

    Intentando añadir actions

---
+++ src/actions/index.ts
@@ -0,0 +1,17 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+export const server = {
+  myAction: defineAction({
+    comentario: z.object({
+      texto: z.string().min(10, "El comentario no puede contener menos de 10 caracteres").max(250, "El comentario no puede contener más de 250 caracteres"),
+      nombre: z.string().min(3, "El nombre no puede contener menos de 3 caracteres").max(25, "El nombre no puede contener más de 25 caracteres"),
+    }),
+    handler: async (comentario) => {
+      return {
+        success: true,
+        message: "Gracias por tu comentario!"
+      };
+    }
+  }
+)};
